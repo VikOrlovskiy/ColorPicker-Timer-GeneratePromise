66e5c55794623affca576c6f479e40e781708b9a
vik_orel <vikorel34@gmail.com>
2021-11-19 10:00:27 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+// =======================================================================
+refs.startBtn.disabled = false;
+let timerID = null;
+// =======================================================================
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  changeRandomBGColor();
+});
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = false;
+  clearInterval(timerID);
+});
+// =======================================================================
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// =======================================================================
+function changeRandomBGColor() {
+  timerID = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    console.log(getRandomHexColor());
+  }, 1000);
+}
