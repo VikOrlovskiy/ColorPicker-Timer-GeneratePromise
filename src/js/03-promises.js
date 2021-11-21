fa8e6b87d003a46ec1dfb9d49f0737c083514ca5
vik_orel <vikorel34@gmail.com>
2021-11-21 17:55:02 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+const refs = {
+  form: document.querySelector('form'),
+};
+refs.form.addEventListener('submit', e => {
+  console.log(e.target);
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
