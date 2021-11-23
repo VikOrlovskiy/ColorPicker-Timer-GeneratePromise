9ee544771dee1f3404000c33d6e4bdab23fb9415
vik_orel <vikorel34@gmail.com>
2021-11-23 16:51:07 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,40 @@
+import Notiflix from 'notiflix';
+// ================================================================
 const refs = {
   form: document.querySelector('form'),
+  inputs: document.querySelectorAll('input'),
 };
-refs.form.addEventListener('submit', e => {
-  console.log(e.target);
-});
-
+let inputsValue = [];
+let timerId = null;
+let counter = 0;
+// =============================================================================
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+}
+// =============================================================================
+refs.form.addEventListener('submit', startIvents);
+// =======================================
+function startIvents(e) {
+  e.preventDefault();
+  refs.inputs.forEach(getInputsValue);
+  let [delay, step, amount] = inputsValue;
+
+  timerId = setInterval(() => {
+    counter += 1;
+    if (amount <= counter) {
+      clearInterval(timerId);
+    }
+    delay += step;
+    createPromise(counter, delay);
+    console.log(counter);
+  }, delay);
+}
+// ===================================================
+function getInputsValue(input) {
+  return inputsValue.push(Number(input.value));
 }
